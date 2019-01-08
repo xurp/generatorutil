96c1db7e4df33dd72add8392f07faeda250e1f71
wuyantao <yantao.wu@lianzhongjinrong.cn>
2019-01-08 17:25:58 +0800

    idea mybatis mysql 生成逆向工程

---
+++ src/main/java/com/example/generatorutil/GeneratorutilApplication.java
@@ -0,0 +1,14 @@
+package com.example.generatorutil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorutilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratorutilApplication.class, args);
+    }
+
+}
+
